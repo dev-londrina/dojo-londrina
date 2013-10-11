68b474907f8c5ffa060e926d3120c0e257f6ce12
Michel Banagouro <mbanagouro@outlook.com>
2013-10-11 06:40:50 -0300

    jokenpo em csharp completo

---
+++ 9-jokenpo/Dojo-Juntus/Jokenpo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dojo_Juntus
+{
+    class Jokenpo
+    {
+        public Dictionary<string, string> Regras
+        {
+            get
+            {
+                var ganha = new Dictionary<string, string>();
+                ganha.Add("pedra", "tesoura");
+                ganha.Add("papel", "pedra");
+                ganha.Add("tesoura", "papel");
+                return ganha;
+            }
+        }
+
+        public int Jogar(string jogada1, string jogada2)
+        {
+
+            verificarJogadasValidas(jogada1, jogada2);
+
+            if (jogada1 == jogada2) return 0;
+
+            return JogadaUmGanha(jogada1, jogada2) ? 1 : 2;
+        }
+
+        private bool JogadaUmGanha(string jogada1, string jogada2)
+        {
+            return Regras[jogada1] == jogada2;
+        }
+
+        private void verificarJogadasValidas(string jogada1, string jogada2)
+        {
+            if (!Regras.ContainsKey(jogada1) || !Regras.ContainsKey(jogada2))
+                throw new ArgumentException();
+        }
+    }
+}
---
+++ 9-jokenpo/Dojo-Juntus/UnitTest1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dojo_Juntus
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void pedra_com_pedra_dah_empate()
+        {
+            var jokenpo = new Jokenpo();
+            var resultado = jokenpo.Jogar("pedra", "pedra");
+
+            Assert.AreEqual(0, resultado);
+        }
+
+        [TestMethod]
+        public void pedra_com_tesoura_dah_jogador_1()
+        {
+            var jokenpo = new Jokenpo();
+            var resultado = jokenpo.Jogar("pedra", "tesoura");
+
+            Assert.AreEqual(1, resultado);
+        }
+
+        [TestMethod]
+        public void pedra_com_papel_dah_jogador_2()
+        {
+            var jokenpo = new Jokenpo();
+            var resultado = jokenpo.Jogar("pedra", "papel");
+
+            Assert.AreEqual(2, resultado);
+        }
+
+        [TestMethod]
+        public void tesoura_com_papel_dah_jogador_1()
+        {
+            var jokenpo = new Jokenpo();
+            var resultado = jokenpo.Jogar("tesoura", "papel");
+
+            Assert.AreEqual(1, resultado);
+        }
+
+        [TestMethod]
+        public void tesoura_com_tesoura_dah_empate()
+        {
+            var jokenpo = new Jokenpo();
+            var resultado = jokenpo.Jogar("tesoura", "tesoura");
+
+            Assert.AreEqual(0, resultado);
+        }
+
+        [TestMethod]
+        public void tesoura_com_pedra_dah_jogador_2()
+        {
+            var jokenpo = new Jokenpo();
+            var resultado = jokenpo.Jogar("tesoura", "pedra");
+
+            Assert.AreEqual(2, resultado);
+        }
+
+        [TestMethod]
+        public void papel_com_pedra_dah_jogador_1()
+        {
+            var jokenpo = new Jokenpo();
+            var resultado = jokenpo.Jogar("papel", "pedra");
+
+            Assert.AreEqual(1, resultado);
+        }
+
+        [TestMethod]
+        public void papel_com_tesoura_dah_jogador_2()
+        {
+            var jokenpo = new Jokenpo();
+            var resultado = jokenpo.Jogar("papel", "tesoura");
+
+            Assert.AreEqual(2, resultado);
+        }
+
+
+        [TestMethod]
+        public void papel_com_papel_dah_empate()
+        {
+            var jokenpo = new Jokenpo();
+            var resultado = jokenpo.Jogar("papel", "papel");
+
+            Assert.AreEqual(0, resultado);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void agua_com_vinagre_dah_merda()
+        {
+            var jokenpo = new Jokenpo();
+            var resultado = jokenpo.Jogar("agua", "vinagre");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void agua_com_agua_dah_merda()
+        {
+            var jokenpo = new Jokenpo();
+            var resultado = jokenpo.Jogar("agua", "agua");
+        }
+    }
+}
