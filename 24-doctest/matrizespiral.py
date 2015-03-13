f25c9b0b19e20eb75dbe8c06d49539e400922d65
Rafael Arabori <arabori@gmail.com>
2015-03-13 00:16:00 -0300

    Adiciona condicoes nos casos existentes, para considerar as direcoes

--- 24-doctest/matrizespiral.py
+++ 24-doctest/matrizespiral.py
@@ -18,6 +18,12 @@ class MatrizEspiral:
         [[1], [2]]
         >>> me.entrada(3, 3)
         [[1, 2, 3], [8, 9, 4], [7, 6, 5]]
+        >>> me.entrada(3, 4)
+        [[1, 2, 3, 4], [10, 11, 12, 5], [9, 8, 7, 6]]
+        >>> me.entrada(4, 3)
+        [[1, 2, 3], [10, 11, 4], [9, 12, 5], [8, 7, 6]]
+        >>> me.entrada(6, 5)
+        [[1, 2, 3, 4, 5], [18, 19, 20, 21, 6], [17, 28, 29, 22, 7], [16, 27, 30, 23, 8], [15, 26, 25, 24, 9], [14, 13, 12, 11, 10]]
         """
         matriz = [[ 0 for x in range(coluna) ] for y in range (linha)]
         i = 0
@@ -29,10 +35,10 @@ class MatrizEspiral:
             matriz[i][j] = n + 1
 
             muda_direcao = (
-                j == coluna -1 or
-                i == linha  -1 or
-                (j == 0 and i != 0) or
-                matriz[i + incremento_linha][j + incremento_coluna] != 0
+                (j == coluna -1 and direcao == 1) or
+                (i == linha  -1 and direcao == 2) or
+                (j == 0 and direcao == 3) or
+                (n > 1 and matriz[i + incremento_linha][j + incremento_coluna] != 0)
             )
 
             if muda_direcao:
@@ -52,6 +58,9 @@ class MatrizEspiral:
             elif direcao == 4:
                 incremento_linha = -1
                 incremento_coluna = 0
+
+            i+=incremento_linha
+            j+=incremento_coluna
 
         return matriz
 
