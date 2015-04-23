21d8a241bf2c16ca6847927e270b6505b29b5ade
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2015-04-23 03:18:07 -0300

    Fix use of luaunit 2.1 (installed from luarocks) and use it's function assertEquals

--- 25-sudoku/TestSudoku.lua
+++ 25-sudoku/TestSudoku.lua
@@ -1,63 +1,67 @@
 require('Sudoku')
-luaunit = require('luaunit')
+lu = require('luaunit')
 
 TestSudoku = {}
 
 function TestSudoku:setUp()
-	sudoku = Sudoku.new()
+    sudoku = Sudoku.new()
 end
 
 function TestSudoku:testQuadradoVazioValido()
-	assert(sudoku:isValid({{0, 0, 0},{0, 0, 0},{0, 0, 0}}))
+    assertEquals(sudoku:isValid({{0, 0, 0},{0, 0, 0},{0, 0, 0}}), true)
 end
 
 function TestSudoku:testQuadradoIncompletoInvalido()
-	assert(not sudoku:isValid({{},{}}))
+    assertEquals(sudoku:isValid({{},{}}), false)
 end
 
 function TestSudoku:testQuadradoCompletoValido()
-	assert(sudoku:isValid({{1,2,3} , {4,5,6} , {7,8,9}}))
+    assertEquals(sudoku:isValid({{1,2,3} , {4,5,6} , {7,8,9}}), true)
 end
 
 function TestSudoku:testQuadradoCompletoInvalido()
-	assert(not sudoku:isValid({{1,2,3,4} , {4,5,6} , {7,8,9}}))
+    assertEquals(sudoku:isValid({{1,2,3,4} , {4,5,6} , {7,8,9}}), false)
 end
 
 function TestSudoku:testQuadradoCompletoRepetido()
-	assert(not sudoku:isValid({{1,2,4} , {4,5,6} , {7,8,9}}))
+    assertEquals(sudoku:isValid({{1,2,4} , {4,5,6} , {7,8,9}}), false)
 end
 
 function TestSudoku:testNumerosValidos()
-	assert(not sudoku:isValid({{1,2,3} , {4,5,6} , {7,8,10}}))
+    assertEquals(sudoku:isValid({{1,2,3} , {4,5,6} , {7,8,10}}), false)
 end
 
 function TestSudoku:testLinhaValida()
-	assert(sudoku:isValidRow({1,2,3,4,5,6,7,8,9}))
+    assertEquals(sudoku:isValidRow({1,2,3,4,5,6,7,8,9}), true)
 end
 
 function TestSudoku:testLinhaMenorInvalida()
-	assert(not sudoku:isValidRow({1,2,3,4,5,6,7,8}))
+    assertEquals(sudoku:isValidRow({1,2,3,4,5,6,7,8}), false)
 end
 
 function TestSudoku:testLinhaNumeroRepetido()
-	assert(not sudoku:isValidRow({1,2,3,3,4,5,6,7,8}))
+    assertEquals(sudoku:isValidRow({1,2,3,3,4,5,6,7,8}), false)
 end
 
 function TestSudoku:testLinhaNumerosInvalidos()
-	assert(not sudoku:isValidRow({1,2,3,10,4,5,6,7,8}))
+    assertEquals(sudoku:isValidRow({1,2,3,10,4,5,6,7,8}), false)
 end
 
 function TestSudoku:test9x9Valido()
-	assert(sudoku:isValid9x9({
-		{1,2,3,4,5,6,7,8,9},
-		{0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0}}))
+    assertEquals(
+        sudoku:isValid9x9({
+            {1,2,3,4,5,6,7,8,9},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0}
+        }),
+        true
+    )
 end
 
-luaunit:run()
+lu:run()
