4f8c0f4421c66332b43bb6130b03eb2da0cebdac
fefno <fefno@hotmail.com>
2015-07-02 11:24:08 -0300

    27 - assassino

---
+++ 27 - Assassino/Dojo1806/Solucao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dojo1806
+{
+    public class Solucao
+    {
+        public int assasino;
+        public int local;
+        public int arma;
+
+        public Solucao(int assasino, int local, int arma)
+        {
+            this.assasino = assasino;
+            this.local = local;
+            this.arma = arma;
+        }
+
+        public override bool Equals(object obj)
+        {
+
+            var that = obj as Solucao;
+            if (obj == null)
+            {
+                return false;
+            }
+            return this.arma == that.arma && this.assasino == that.assasino && this.local == that.local;
+        }
+    }
+}
---
+++ 27 - Assassino/Dojo1806/Testemunha.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dojo1806
+{
+    public class Testemunha
+    {
+        private int assassino;
+        private int local;
+        private int arma;
+
+        public Testemunha(int p1, int p2, int p3)
+        {
+            // TODO: Complete member initialization
+            this.assassino = p1;
+            this.local = p2;
+            this.arma = p3;
+        }
+
+
+        public int testaTeoria(int p1, int p2, int p3)
+        {
+            List<int> list = new List<int>();
+            if (p1 != assassino)
+                list.Add(1);
+            if (p2 != local)
+                list.Add(2);
+            if (p3 != arma)
+                list.Add(3);
+
+            if (list.Count() == 0)
+                return 0;
+            else
+                return list[new Random().Next(0, list.Count())];
+        }
+    }
+}
